e4860a09c2cfa998e5a3f5cac74b9b5c3d169f44
Abhi <73659497+abhitanu@users.noreply.github.com>
2022-06-11 14:34:35 -0400

    Update streamlit_app.py
    Use double quotes for file names

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-myFile = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+myFile = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataFrame(myFile)
