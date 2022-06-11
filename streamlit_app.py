0a88146cd0d57f65814afc452d53a939f994ae0d
Abhi <73659497+abhitanu@users.noreply.github.com>
2022-06-11 14:24:28 -0400

    Update streamlit_app.py
    Let try git comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled range free Eggs')
+streamlit.text('Hard boiled range free brown Eggs')
 
