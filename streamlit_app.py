56bdd87a4f1a480b36209a457b29fbf6cd92be2c
Abhi <73659497+abhitanu@users.noreply.github.com>
2022-06-11 14:35:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 myFile = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataFrame(myFile)
+streamlit.dataframe(myFile)
